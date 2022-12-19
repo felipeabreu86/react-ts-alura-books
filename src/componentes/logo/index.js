56b3b28e05d9d71c3ccbec1f08bbaad8467458d3
FELIPE MARTINS ABREU <felipe.abreu@trt1.jus.br>
2022-12-19 09:02:22 -0300

    estilizando

--- src/componentes/logo/index.js
+++ src/componentes/logo/index.js
@@ -4,7 +4,7 @@ import estilo from './estilo.module.css';
 export default function Logo() {
     return (
         <div className={estilo.logo}>
-            <img src={logo} alt='logotipo' />
+            <img src={logo} alt='logotipo' className={estilo.logoImg} />
             <p><strong>Alura</strong>Books</p>
         </div >
     );
