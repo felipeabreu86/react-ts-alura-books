ff71d75ad276f877745c7bf38d588da64ef9fc98
FELIPE MARTINS ABREU <felipe.abreu@trt1.jus.br>
2022-12-19 09:20:27 -0300

    estilizando o header

--- src/telas/App/App.tsx
+++ src/telas/App/App.tsx
@@ -1,11 +1,28 @@
-import './App.css';
+import estilo from './App.module.css';
 import Logo from './../../componentes/logo';
+import perfil from './../../imagens/perfil.svg';
+import sacola from './../../imagens/sacola.svg';
+
+
+const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE'];
+const icones = [perfil, sacola];
 
 export default function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
+    <div className={estilo.App}>
+      <header className={estilo.AppHeader}>
         <Logo />
+        <ul className={estilo.opcoes}>
+          {textoOpcoes.map((texto) => (
+            <li className={estilo.opcao}><p>{texto}</p></li>
+          ))}
+        </ul>
+
+        <ul className={estilo.icones}>
+          {icones.map((icone) => (
+            <li className={estilo.icone}><img src={icone} /></li>
+          ))}
+        </ul>
       </header>
     </div>
   );
