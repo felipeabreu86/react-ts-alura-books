78060c8c846bb4606396e9120fe8ced9c6da27ca
FELIPE MARTINS ABREU <felipe.abreu@trt1.jus.br>
2022-12-19 10:37:12 -0300

    adicionando o pacote styled components e realizando a conteinerização do App e do Header

--- src/componentes/Header/index.tsx
+++ src/componentes/Header/index.tsx
@@ -3,18 +3,24 @@ import Logo from "../Logo";
 import OpcoesHeader from "./OpcoesHeader";
 import perfil from './../../imagens/perfil.svg';
 import sacola from './../../imagens/sacola.svg';
-import estilo from './estilo.module.css';
+import styled from 'styled-components';
 
+
+const HeaderContainer = styled.header`
+    background-color: #FFF;
+    display: flex;
+    justify-content: center;
+`
 
 const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE'];
 const icones = [perfil, sacola];
 
 export default function Header() {
     return (
-        <header className={estilo.AppHeader}>
+        <HeaderContainer>
             <Logo />
             <OpcoesHeader textoOpcoes={textoOpcoes} />
             <IconesHeader icones={icones} />
-        </header>
+        </HeaderContainer>
     );
 }
--- src/telas/App/index.tsx
+++ src/telas/App/index.tsx
@@ -1,11 +1,20 @@
 import Header from '../../componentes/Header';
-import estilo from './estilo.module.css';
+import styled from 'styled-components';
 
+const AppContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(90deg, #002F52 35%, #326589);
+
+  li {
+    list-style: none;
+  }
+`
 
 export default function App() {
   return (
-    <div className={estilo.App}>
+    <AppContainer>
       <Header />
-    </div>
+    </AppContainer>
   );
 }
